782d17be30f1849a29f3ca27efb9f3e36d6ccf66
lab314-32@labeng.local <lab314-32@labeng.local>
2020-12-29 15:27:49 +0700

    completed

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,11 +1,49 @@
-/*
-"Enter your age: "
-"Enter your height: "
-"Enter your property: "
-"Your status = "
-"UNFRIEND"
-"FRIEND"
-"BEST FRIEND"
-"ONE-NIGHT-STAND"
-"MARRIED"
-*/
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    int age,height;
+    long int money;
+    string status;
+    cout << "Enter your age: ";
+    cin >> age;
+    if(age <= 20){
+        cout << "Enter your height: ";
+        cin >> height;
+        if(height < 160){
+            status = "UNFRIEND";
+        }
+        else if(height < 175){
+            status = "FRIEND";
+        }
+        else{
+            cout << "Enter your property: ";
+            cin >> money;
+            if(money > 69000000){
+                status = "MARRIED";
+            }
+            else{
+                status = "ONE-NIGHT-STAND";
+            }
+        }
+    }
+    else if(age < 30){
+        cout << "Enter your property: ";
+        cin >> money;
+        if(money > 10000000){
+            status = "BEST FRIEND";
+        }
+        else{
+            status = "UNFRIEND";
+        }
+    }
+    else{
+        status = "UNFRIEND";
+    }
+    cout << "Your status = " << status;
+
+	return 0;
+
+}
